0d075b5c9b7d718cee26b7984410ce1b1ffb1b15
Abdullah Shuja <4b5huja@gmail.com>
2018-06-13 01:34:32 +0100

    inheritance, multiple inheritance

--- OOP/classes.py
+++ OOP/classes.py
@@ -1,9 +1,48 @@
-class SmartPhone:
-    def __init__(self, model, brand):
-        self.model = model
-        self.brand = brand
+class Animal:
+    def __init__(self, habitat, diettype):
+        self.habitat = habitat
+        self.diettype = diettype
+
+    def printHabitat(self):
+        print(self.habitat)
 
 
-iphonesix = SmartPhone("iPhone 6", "Apple")
-print(iphonesix.model)
-print(iphonesix.brand)
+lion = Animal("jungle", "carnivore")
+print("Habitat: {}\nDiet: {}".format(lion.habitat, lion.diettype))
+
+
+# Inheritance
+class Fish(Animal):
+    def __init__(self, colour, diettype, habitat="sea"):
+        super().__init__(habitat, diettype)
+        self.colour = colour
+
+
+clownfish = Fish("orange", "omnivore")
+
+# use isinstance to check class types
+print(isinstance(clownfish, Fish))
+# and issubclass to check for subclass
+print(issubclass(Fish, Animal))
+
+
+class Character():
+    def __init__(self, name):
+        self.name = name
+
+    def printName(self):
+        print(self.name)
+
+
+# Multiple Inheritance
+class NemoCharacter(Fish, Character):
+    def __init__(self, colour, diettype, name):
+        self.colour = colour
+        self.diettype = diettype
+        self.name = name
+        self.habitat = "sea"
+
+
+marlin = NemoCharacter("orange", "omnivore", "Marlin")
+marlin.printHabitat()  # <- this method comes from Animal, which is extended by Fish
+marlin.printName()  # <- this method comes from Character
