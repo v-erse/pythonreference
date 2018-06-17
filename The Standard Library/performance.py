5e1421422c2f9ca1dbc555ce08026106b248b137
Abdullah Shuja <4b5huja@gmail.com>
2018-06-17 22:30:12 +0200

    performance

---
+++ The Standard Library/performance.py
@@ -0,0 +1,16 @@
+# We can use the timeit module (importing the Timer class), to test how long
+# our functions take to execute, allowing us to assess performance easily
+from timeit import Timer
+
+
+def factorial(n):
+    if n > 0:
+        return n + factorial(n-1)
+    else:
+        return 0
+
+
+# First we make a Timer instance. The first parameter is the statement that is
+# to be tested, the second parameter is setup code
+test = Timer("factorial(10)", "from __main__ import factorial")
+print(test.timeit())
