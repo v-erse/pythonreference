439926c7041b5439b6e62b5059978579067cf681
Abdullah Shuja <4b5huja@gmail.com>
2018-06-24 14:09:30 +0200

    FizzBuzz in two lines!!

--- Numpy/fizzbuzzwithnumpy.py
+++ Numpy/fizzbuzzwithnumpy.py
@@ -1,6 +1,11 @@
 import numpy as np
-
+# four lines
 x = np.arange(1, 101)
 condlist = [(x % 3 == 0) & (x % 5 == 0), x % 3 == 0, x % 5 == 0, x]
 choicelist = ["fizzbuzz", "fizz", "buzz", x]
 print(np.select(condlist, choicelist))
+
+# faster!
+x = np.arange(1, 101)
+print(np.select([(x % 3 == 0) & (x % 5 == 0), x % 3 == 0, x % 5 == 0],
+                ["fizzbuzz", "fizz", "buzz"], default=x))
